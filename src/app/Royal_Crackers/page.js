38229b1f475488b2053717926baea355eb9fa9d2
David <davidsedarous@cmail.cartleon.ca>
2025-01-20 17:49:34 -0500

    Added image gallery for Royal Crackers page

--- src/app/Royal_Crackers/page.js
+++ src/app/Royal_Crackers/page.js
@@ -1,10 +1,86 @@
+"use client";
 import { VerticalNav, HorizontalNav } from "../components/verticalnav";
+import { ImageGallery } from "react-image-grid-gallery";
 import {
   SoloWideImage,
   DuoImage,
   GalleryImage,
 } from "../components/imageformatting";
 import one from "./One.png";
+
+const imagesArray = [
+  {
+    id: "One",
+    alt: "RC1",
+    caption: "Royal Crackers - Photo Box and Photo Stack",
+    src: "/Royal_Crackers/1.png",
+  },
+  {
+    id: "Two",
+    alt: "RC2",
+    caption: "Royal Crackers - Fajita and Broken Plate Iterations",
+    src: "/Royal_Crackers/2.png",
+  },
+  {
+    id: "Three",
+    alt: "RC3",
+    caption: "Royal Crackers - Sleep Number Envelope",
+    src: "/Royal_Crackers/3.png",
+  },
+  {
+    id: "Four",
+    alt: "RC4",
+    caption: "Royal Crackers - Platter of Fancy Food",
+    src: "/Royal_Crackers/4.png",
+  },
+  {
+    id: "Five",
+    alt: "RC5",
+    caption: "Royal Crackers - Stebe Towel Actions (1)",
+    src: "/Royal_Crackers/5.png",
+  },
+  {
+    id: "Six",
+    alt: "RC6",
+    caption: "Royal Crackers - Stebe Towel Actions (2)",
+    src: "/Royal_Crackers/6.png",
+  },
+  {
+    id: "Seven",
+    alt: "RC7",
+    caption: "Royal Crackers - Pillow Poses",
+    src: "/Royal_Crackers/7.png",
+  },
+  {
+    id: "Eight",
+    alt: "RC2",
+    caption: "Royal Crackers - Stebe Bib",
+    src: "/Royal_Crackers/8.png",
+  },
+  {
+    id: "Nine",
+    alt: "RC9",
+    caption: "Royal Crackers - Fast Food Baskets",
+    src: "/Royal_Crackers/9.png",
+  },
+];
+
+const custStyle = {
+  opacity: 0,
+  fontSize: "0.85rem",
+  transition: "opacity 1s ease-in-out",
+  position: "absolute",
+  bottom: "0px",
+  zIndex: 1000,
+  width: "100%",
+  background: "linear-gradient(rgba(25, 27, 38, 0), rgb(25, 27, 38))",
+  padding: "16px",
+  textAlign: "center",
+  color: "rgb(255, 255, 255)",
+  fontFamily: "Avenir",
+  paddingTop: "60px",
+};
+
 export default function Home() {
   return (
     <div className="overflow-x-hidden flex w-screen h-screen">
@@ -24,9 +100,13 @@ export default function Home() {
           misadventures along the way.
         </p>
         <h2 className="pt-4 text-2xl font-titled text-gray-800">Prop Design</h2>
-
-        {/* <Image src={one} alt={"one"} /> */}
-        <GalleryImage srcOne={"/One.png"} altOne="box" />
+        <ImageGallery
+          imagesInfoArray={imagesArray}
+          columnCount={3}
+          columnWidth={230}
+          gapSize={8}
+          customStyles={{ imageCaptionStyle: custStyle }}
+        />
       </div>
     </div>
   );
