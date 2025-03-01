f9b8784bfddd1d9fb9a91a4a6660b954b78e51ec
David <davidsedarous@cmail.cartleon.ca>
2025-02-28 21:04:06 -0500

    updated favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,7 +27,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-white h-screen">
-      <link rel="icon" href="/icon?" />
       <title>Emma Sawyer Art</title>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
